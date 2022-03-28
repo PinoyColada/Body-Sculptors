fa658a4c62204f742f1ec969e3133e94bd557c0d
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-28 15:46:28 -0400

    added imports(express,db,cors,etc.) to server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const { Router } = require('express');
-const controllers = require('./controllers');
-const router = Router();
+const express = require('express');
+const routes = require('./routes');
+const db = require('./db');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
+const PORT = process.env.PORT || 3001;
 
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
 
-module.exports = router;
+app.use('/', routes);
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
