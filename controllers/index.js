eea017ba8412d1e6c5e51ab1fad5d22921fb5e63
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-28 14:10:49 -0400

    added get and post controllers for user and workout

---
+++ controllers/index.js
@@ -0,0 +1,44 @@
+const { User, Workout } = require('../models/index')
+
+
+const createUser = async (req, res) => {
+    try {
+        const user = await new User(req.body)
+        await user.save()
+        return res.status(201).json({
+            user,
+        });
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find()
+        return res.status(200).json({ users })
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const createWorkout = async (req, res) => {
+    try {
+        const workout = await new Workout(req.body)
+        await workout.save()
+        return res.status(201).json({
+            workout,
+        });
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const getAllWorkout = async (req, res) => {
+    try {
+        const workouts = await Workout.find()
+        return res.status(200).json({ workouts })
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
