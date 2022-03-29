d258cc2deb7474a22db87b85253eb71ddd2e8da1
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-29 18:21:17 -0400

    add logo and stylized navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,16 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <nav class = "navbar-style">
-            <div> LOGO </div>
-            <ul>
-                <li><Link to='/' className='navWord first'>Main Page</Link></li>
-                <li><Link to='/createUser' className='navWord'>Create a workout</Link></li>
-                <li><Link to='/users' className='navWord'>View workout history</Link></li>
+        <nav class="navbar-style">
+            <div>
+                <Link to='/' className='navWord first'>
+                    <img className="logo"src="https://i.ibb.co/mtTVYYz/Screen-Shot-2022-03-29-at-6-02-33-PM.png" alt="Body-Sculptors Logo"/>
+                </Link>
+            </div>
+            <ul className="navbar-ulist-style">
+                <li className="navbar-list-style"><Link to='/createUser' className='navWord'>Create a workout</Link></li>
+                <li className="navbar-list-style"><Link to='/users' className='navWord'>View workout history</Link></li>
+                <li className="navbar-list-style"><Link to='/aboutMe' className='navWord first'>About me</Link></li>
             </ul>
         </nav>
     )
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const Landing = () => {
 
     return (
         <div className='landing'>
+            <Navbar />
             <h1>Body Sculptors</h1>
             <Link to ='/createUser'>Create a User</Link>
         </div>
