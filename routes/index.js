a29c8282732990ced581ce3b797ab40a8359841f
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-29 12:24:38 -0400

    added get routes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const controllers = require('../controllers');
 const router = Router();
 
 router.post('/createUser', controllers.createUser);
-
 router.post('/createWorkouts', controllers.createWorkout);
+router.get('/getUsers', controllers.getAllUsers);
+router.get('/getWorkouts', controllers.getAllWorkout);
 
 
 module.exports = router;
