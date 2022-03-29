fa581e5c4a250b49f2467efa8a4c773a34d62eda
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-29 15:33:14 -0400

    configure for heroku deployment

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(bodyParser.json());
 app.use(cors())
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/', routes);
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
