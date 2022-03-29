3c881046bb305c871ede8c09d01b3b6ffc326122
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-29 14:05:21 -0400

    created main page that links to user creation page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 const Landing = () => {
 
     return (
         <div className='landing'>
+            <h1>Body Sculptors</h1>
+            <Link to ='/createUser'>Create a User</Link>
         </div>
     )
 
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const router = Router();
 
 router.post('/createUser', controllers.createUser);
 router.post('/createWorkouts', controllers.createWorkout);
-router.get('/getUsers', controllers.getAllUsers);
-router.get('/getWorkouts', controllers.getAllWorkout);
+router.get('/users', controllers.getAllUsers);
+router.get('/workouts', controllers.getAllWorkout);
 
 
 module.exports = router;
