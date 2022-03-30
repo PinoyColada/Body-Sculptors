5f5a97f3043be9e6761483ccacdec5580b9abf8d
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-30 13:51:17 -0400

    fixed post route for user creation

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const controllers = require('../controllers');
 const router = Router();
 
-router.post('/createUser', controllers.createUser);
+router.post('/userCreation', controllers.createUser);
 router.post('/createWorkouts', controllers.createWorkout);
 router.get('/users', controllers.getAllUsers);
 router.get('/workouts', controllers.getAllWorkout);
