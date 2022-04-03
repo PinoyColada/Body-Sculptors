2d680b704493ca5214651868b9a4982c8b4420d6
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-04-03 15:33:28 -0400

    updated routes to website

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const { Router } = require('express');
 const controllers = require('../controllers');
 const router = Router();
 
-router.post('https://agile-journey-75895.herokuapp.com/userCreation', controllers.createUser);
-router.post('https://agile-journey-75895.herokuapp.com/workoutCreation', controllers.createWorkout);
-router.get('https://agile-journey-75895.herokuapp.com/workoutCreation', controllers.getAllUsers);
-router.get('https://agile-journey-75895.herokuapp.com/workoutList', controllers.getAllUsers);
-router.get('https://agile-journey-75895.herokuapp.com/workoutList/:id/workouts', controllers.getAllUserWorkouts);
-router.delete('https://agile-journey-75895.herokuapp.com/workoutList/:id', controllers.deleteUser);
-router.put("https://agile-journey-75895.herokuapp.com/workoutList/:id", controllers.updateUser);
+router.post('/userCreation', controllers.createUser);
+router.post('/workoutCreation', controllers.createWorkout);
+router.get('/workoutCreation', controllers.getAllUsers);
+router.get('/workoutList', controllers.getAllUsers);
+router.get('/workoutList/:id/workouts', controllers.getAllUserWorkouts);
+router.delete('/workoutList/:id', controllers.deleteUser);
+router.put("/workoutList/:id", controllers.updateUser);
 
 
 module.exports = router;
