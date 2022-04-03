03c78d7fa2abe9c40ad2f073833756c1af85daf7
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-04-03 19:06:27 -0400

    removed ':id' from delete and put routes

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ router.post('/workoutCreation', controllers.createWorkout);
 router.get('/workoutCreation', controllers.getAllUsers);
 router.get('/workoutList', controllers.getAllUsers);
 router.get('/workoutList/:id/workouts', controllers.getAllUserWorkouts);
-router.delete('/workoutList/:id', controllers.deleteUser);
-router.put("/workoutList/:id", controllers.updateUser);
+router.delete('/workoutList', controllers.deleteUser);
+router.put("/workoutList", controllers.updateUser);
 
 
 module.exports = router;
